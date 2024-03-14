244dc25d6f2267422bd88b44f00bc3dd0bd8f8c7
raksit <raksit@users.noreply.github.com>
2024-03-14 11:08:12 +0700

    Change django version

--- drt/urls.py
+++ drt/urls.py
@@ -3,15 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.urls')),
+    # path('api/', include('app.urls')),
 ]
-
-# from django.contrib import admin
-# from django.urls import path, include
-# from app import views
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     # path('api/stores/', views.StoreList.as_view()),
-#     # path('api/tasks/<int:pk>', views.TaskDetailUpdateDelete.as_view())
-# ]
