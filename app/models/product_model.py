2c4d0a71d8d41fc7a38ac0a57da0e4c7342e1fa0
raksit <raksit@users.noreply.github.com>
2024-03-21 14:34:44 +0700

    Add timestamp in product

--- app/models/product_model.py
+++ app/models/product_model.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 class Product(models.Model):
     product_id = models.IntegerField(unique=True)
     product_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.product_id} - {self.product_name}"
